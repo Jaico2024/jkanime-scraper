4a7d5eb78b83498f28e1e38ed5466d513ad9266a
Jaico2024 <161821541+Jaico2024@users.noreply.github.com>
2025-07-31 18:44:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+// server.js
 import express from "express";
 import handler from "./api/search.js";
+import capturaHandler from "./api/captura.js";
 
 const app = express();
 
 app.get("/api/search", handler);
+app.get("/api/captura", capturaHandler);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
