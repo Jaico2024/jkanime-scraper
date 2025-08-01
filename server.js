5d22b1531c077911b5d78543738a09459848b7b9
Jaico2024 <161821541+Jaico2024@users.noreply.github.com>
2025-08-01 12:01:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ import express from "express";
 
 const app = express();
 
-// Ruta raíz para comprobar que el servidor está activo
+// Ruta raíz para comprobar estado del servidor
 app.get("/", (req, res) => {
   res.send("✅ Servidor JKAnime Scraper activo");
 });
 
-// Puerto dinámico proporcionado por Railway o 3000 por defecto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor escuchando en el puerto ${PORT}`);
